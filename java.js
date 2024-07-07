0f0c87f46c055183eccd0cb83d04f26a1a79ad0b
kanecarl1996 <63940735+kanecarl1996@users.noreply.github.com>
2024-07-07 09:03:58 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+
+const button1 = document.querySelectorAll("#button1");
+
+button1.onclick = buttonClicked
+
+function buttonClicked(){
+    return button1.innerText = "Button has been Clicked";
+}
+
+
